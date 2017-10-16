1e41f24d0abde652ae2f4cc84948eabeeaf625d5
lukozog <lukaszozog2@gmail.com>
2017-10-16 21:32:20 +0200

    removed wrong solution from CommentController

--- src/main/java/junior/academy/controller/CommentController.java
+++ src/main/java/junior/academy/controller/CommentController.java
@@ -2,7 +2,6 @@ package junior.academy.controller;
 
 import junior.academy.domain.Comment;
 import junior.academy.service.CommentService;
-import junior.academy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +16,6 @@ public class CommentController {
     @Autowired
     CommentService commentService;
 
-    //@Autowired
-    //SongService songService;
-
-    @Autowired
-    UserService userService;
-
     @GetMapping("/{commentId}")
     public ResponseEntity<Comment> getComment(@PathVariable long commentId) {
         if (commentService.isCommentPresent(commentId)) {
@@ -36,15 +29,9 @@ public class CommentController {
         return commentService.getAllComments();
     }
 
-    @PostMapping("/{userId}/{songId}")
-    public ResponseEntity createOrUpdateComment(@RequestBody Comment comment, @PathVariable long userId, @PathVariable long songId) {
-        if(userService.getUserById(userId).isPresent() /*&& songService.getSongById(long songId).isPresent()*/){
-            comment.setUser(userService.getUserById(userId).get());
-            //comment.setSong(songService.getSongById(songId).get());
-            commentService.createOrUpdateComment(comment);
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    @PostMapping
+    public void createOrUpdateComment(@RequestBody Comment comment) {
+        commentService.createOrUpdateComment(comment);
     }
 
     @DeleteMapping("/{commentId}")
