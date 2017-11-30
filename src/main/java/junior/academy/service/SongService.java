0d890da728d00e12a429024c009263fbfc669fa0
lukozog <lukaszozog2@gmail.com>
2017-11-30 22:19:37 +0100

    cleaning song service

--- src/main/java/junior/academy/service/SongService.java
+++ src/main/java/junior/academy/service/SongService.java
@@ -52,108 +52,13 @@ public class SongService {
         return new ArrayList<>(Arrays.asList(MusicGenre.values()));
     }
 
-
-    public Set<Song> getRandomSongsByUserPreferences(long userId) {
-        Map<String, Integer> songsByMusicGenre = calculatingUserPreferences(userId);
-        List<Song> allNotRatedSongs = getNotRatedSongs(userId);
-
-        Set<Song> randomSongs = new HashSet<>();
-        for (String name : songsByMusicGenre.keySet()) {
-
-            List<Song> notRatedSongsOfMusicGenre = getNotRatedSongs(userId, name);
-
-
-            for (int i = 0; i < songsByMusicGenre.get(name); i++) {
-                if (notRatedSongsOfMusicGenre.size() != 0) {
-                    Collections.shuffle(notRatedSongsOfMusicGenre);
-                    randomSongs.add(notRatedSongsOfMusicGenre.get(0));
-                    notRatedSongsOfMusicGenre.remove(0);
-                }
-
-                //todo
-                else if (allNotRatedSongs.size() != 0) {
-                    Collections.shuffle(allNotRatedSongs);
-                    randomSongs.add(allNotRatedSongs.get(0));
-                    allNotRatedSongs.remove(0);
-                }
-            }
-        }
-        return randomSongs;
-    }
-
     private List<Song> getNotRatedSongs(long userId, String musicGenre) {
         return songDao.getNotRatedSongs(userId, MusicGenre.valueOf(musicGenre));
-    }
-
-    private List<Song> getNotRatedSongs(long userId) {
-        return songDao.getNotRatedSongs(userId);
-    }
-
-    private Map<String, Integer> calculatingUserPreferences(long userId) {
-        Map<String, Double> sumOfRatesValuePerMusicGenre = getSumOfRatesValuePerMusicGenre(userId);
-        int totalValueOfRates = getTotalValueOfRates(sumOfRatesValuePerMusicGenre);
-        int songCount = 0;
-        Map<String, Integer> songsByMusicGenre = new TreeMap<>();
-        for (String name : sumOfRatesValuePerMusicGenre.keySet()) {
-            Long balancedNumber = Math.round((sumOfRatesValuePerMusicGenre.get(name) / totalValueOfRates) * NUMBER_OF_RECOMMENDED_SONGS);
-            songCount += balancedNumber;
-            songsByMusicGenre.put(name, balancedNumber.intValue());
-        }
-        if (songCount < NUMBER_OF_RECOMMENDED_SONGS) {
-            Map<String, Integer> sortedSongsByMusicGenre =
-                    songsByMusicGenre.entrySet().stream()
-                            .sorted(Entry.comparingByValue())
-                            .collect(Collectors.toMap(Entry::getKey, Entry::getValue,
-                                    (e1, e2) -> e1, LinkedHashMap::new));
-            List<String> indexes = new ArrayList<>(sortedSongsByMusicGenre.keySet());
-            String musicGenreWithBiggestBalanceNumber = indexes.get(indexes.size() - 1);
-            sortedSongsByMusicGenre.put(musicGenreWithBiggestBalanceNumber, sortedSongsByMusicGenre.get(musicGenreWithBiggestBalanceNumber) + 1);
-            return sortedSongsByMusicGenre;
-        }
-        return songsByMusicGenre;
-    }
-
-    private Map<String, Double> getSumOfRatesValuePerMusicGenre(long userId) {
-        List<Song> ratedSongs = getRatedSongs(userId);
-        Map<Long, Integer> mapOfRates = rateService.getSongAndRateValue(userId);
-        Set<String> ratedMusicGenres = getRatedMusicGenres(ratedSongs);
-
-        Map<String, Double> sumRatesValuePerMusicGenre = new HashMap<>();
-        for (String name : ratedMusicGenres) {
-            int numberOfSongsForMusicType = 0;
-            double sumOfRatesForMusicType = 0;
-            for (Song song : ratedSongs) {
-                if (song.getMusicGenre().name().equals(name)) {
-                    numberOfSongsForMusicType++;
-                    sumOfRatesForMusicType += mapOfRates.get(song.getSongId());
-                }
-            }
-            sumRatesValuePerMusicGenre.put(name, sumOfRatesForMusicType * numberOfSongsForMusicType);
-        }
-        return sumRatesValuePerMusicGenre;
     }
 
     private List<Song> getRatedSongs(long userId) {
         return songDao.getRatedSongs(userId);
     }
-
-    private Set<String> getRatedMusicGenres(List<Song> ratedSongs) {
-        Set<String> ratedMusicGenres = new HashSet<>();
-        for (Song s : ratedSongs) {
-            ratedMusicGenres.add(s.getMusicGenre().name());
-        }
-        return ratedMusicGenres;
-    }
-
-    private int getTotalValueOfRates(Map<String, Double> sumOfRatesValuePerMusicGenre) {
-        int totalValueOfRates = 0;
-        for (String name : sumOfRatesValuePerMusicGenre.keySet()) {
-            totalValueOfRates += sumOfRatesValuePerMusicGenre.get(name);
-        }
-        return totalValueOfRates;
-    }
-
-    //MY METHOD FOR RANDOM SONGS
 
     private Map<MusicGenre, List<Song>> getRatedSongsByGenre(long userId) {
         return this.getRatedSongs(userId).stream().collect(Collectors.groupingBy(Song::getMusicGenre));
@@ -194,7 +99,6 @@ public class SongService {
                                 (e1, e2) -> e1, LinkedHashMap::new));
     }
 
-    //METHOD FOR MOST RATED MUSIC GENRE
     public MusicGenre getMostRatedMusicGenre(long userId) {
         return this.getRatedSongs(userId).stream()
                 .collect(Collectors.groupingBy(Song::getMusicGenre, Collectors.counting()))
@@ -202,19 +106,4 @@ public class SongService {
                 .max(Entry.comparingByValue())
                 .map(Entry::getKey).orElse(null);
     }
-//        ArrayList<MusicGenre> musicGenres = this.getMusicGenreTypes();
-//        long maxGenreOccurrences = 0;
-//        long currentGenreOccurrences = 0;
-//        MusicGenre mostRatedMusicGenre = null;
-//        for(MusicGenre musicGenre: musicGenres){
-//           currentGenreOccurrences = songDao.getRatedSongsByUserId(userId).stream().filter(song -> song.getMusicGenre().equals(musicGenre)).count();
-//            System.out.println("CURRENT MUSIC GENRE: "+ musicGenre);
-//            System.out.println("MAX GENRE OCCURENCES: " +maxGenreOccurrences + mostRatedMusicGenre);
-//           if(currentGenreOccurrences > maxGenreOccurrences){
-//               maxGenreOccurrences = currentGenreOccurrences;
-//               mostRatedMusicGenre = musicGenre;
-//           }
-//        }
-//        return mostRatedMusicGenre;
-//    }
 }
