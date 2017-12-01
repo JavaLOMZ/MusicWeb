71cfbf09c07a654a82696827fe97849e2dac6013
Marek <marek.zemb@gmail.com>
2017-12-01 17:35:52 +0000

    backend validator for Author

--- src/main/java/junior/academy/controller/AuthorController.java
+++ src/main/java/junior/academy/controller/AuthorController.java
@@ -2,11 +2,14 @@ package junior.academy.controller;
 
 import junior.academy.domain.Author;
 import junior.academy.service.AuthorService;
+import junior.academy.validator.AuthorValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -16,6 +19,15 @@ public class AuthorController {
 
     @Autowired
     AuthorService authorService;
+
+    @Autowired
+    AuthorValidator authorValidator;
+
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.setValidator(this.authorValidator);
+    }
 
     @GetMapping("/{authorId}")
     public ResponseEntity<Author> getAuthorById(@PathVariable long authorId) {
@@ -31,7 +43,7 @@ public class AuthorController {
     }
 
     @PostMapping()
-    public void createOrUpdateAuthor(@RequestBody Author author) {
+    public void createOrUpdateAuthor(@RequestBody @Valid Author author) {
         authorService.createOrUpdateAuthor(author);
     }
 
@@ -42,5 +54,15 @@ public class AuthorController {
             return new ResponseEntity(HttpStatus.OK);
         }
         return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<Author> findAuthorByName(@PathVariable String name) {
+        Author responseAuthor = authorService.findAuthorByName(name);
+        if (responseAuthor != null) {
+            return new ResponseEntity<>(responseAuthor, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
