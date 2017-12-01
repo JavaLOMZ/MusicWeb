4b62e7505426feef9ee3a083ea2cc4ff2e4f5fb6
Marek <marek.zemb@gmail.com>
2017-12-01 14:45:01 +0000

    backend+fronend createAuthor validation

--- frontend/src/app/author/author.service.ts
+++ frontend/src/app/author/author.service.ts
@@ -22,26 +22,64 @@ export class AuthorService {
   });
 
 
-  getAllAuthors(): Observable<Author[]>  {
+  getAllAuthors(): Observable<Author[]> {
     return this.http.get(this.apiUrl, {headers: this.headers})
-      .map((res:Response) => res.json())
-      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
   }
 
   getAuthorById(authorId: number): Observable<Author> {
-    return this.http.get(this.apiUrl+'/'+authorId, {headers: this.headers})
-      .map((res:Response)=>res.json())
-      .catch((error:any)=>Observable.throw(error.json().error || 'Server Error'));
+    return this.http.get(this.apiUrl + '/' + authorId, {headers: this.headers})
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server Error'));
   }
 
   createOrUpdateAuthor(author: Author): Observable<Author> {
-    return this.http.post(this.apiUrl ,author, {headers: this.headers})
-      .catch((error:any)=> Observable.throw(error.json().error || 'Server error'));
+    return this.http.post(this.apiUrl, author, {headers: this.headers})
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
   }
 
   deleteAuthorById(authorId: number): Observable<boolean> {
     return this.http.delete(this.apiUrl + '/' + authorId, {headers: this.headers})
-      .map((res:Response) => res.json())
-      .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+      .map((res: Response) => res.json())
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  getAuthorByName(name: string): Observable<any> {
+    return this.http.get(this.apiUrl + "/name/" + name, {headers: this.headers})
+      .map(this.extractData)
+      .catch(this.handleError) as Observable<any>;
+  };
+
+  handleError(error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+
+  extractData<T>(res: Response) {
+    let body = res.json();
+    return <T>body;
   }
 }
+  export function authorNameTaken(authorService: AuthorService) {
+    return control => new Promise((resolve, reject) => {
+      console.log("in validator");
+      authorService.getAuthorByName(control.value).subscribe(data => {
+        console.log(data);
+        if (data.authorId) {
+          resolve({authorNameTaken: true})
+        } else {
+          resolve(null);
+        }
+      }, (err) => {
+        console.log("in error" + err);
+        if (err != "404 - Not Found") {
+          resolve(null);
+        } else {
+          resolve(null);
+        }
+      });
+    });
+  }
