131c042e5765f78643f4dfb2c55285fc50e21d52
lukozog <lukaszozog2@gmail.com>
2017-12-31 13:18:07 +0100

    fixed editing song plus some changes about recommended songs look

--- frontend/src/app/song/song-random-list/song-random-list.component.ts
+++ frontend/src/app/song/song-random-list/song-random-list.component.ts
@@ -5,25 +5,32 @@ import {Router} from "@angular/router";
 import {UserService} from "../../user/user.service";
 import {User} from "../../user/user";
 import {AuthenticationService} from "../../authentication.service";
+import {AuthorService} from "../../author/author.service";
+import {Author} from "../../author/author";
 
 @Component({
   selector: 'app-song-random-list',
   templateUrl: './song-random-list.component.html',
   styleUrls: ['./song-random-list.component.css'],
-  providers:[SongService, UserService, AuthenticationService]
+  providers:[SongService, UserService, AuthenticationService, AuthorService]
 })
 export class SongRandomListComponent implements OnInit {
 
   songs:Song[];
   user: User;
   userId: number;
+  authors: Author[];
+  authorName:string;
+
   constructor(private router:Router,
               private songService:SongService,
               private userService: UserService,
-              private authenticationService: AuthenticationService) { }
+              private authenticationService: AuthenticationService,
+              private authorService: AuthorService) { }
 
   ngOnInit() {
     this.getRandomSongsByUserPreferences();
+    this.getAllAuthors();
   }
 
 
@@ -47,6 +54,27 @@ export class SongRandomListComponent implements OnInit {
   redirectSingleSongPage(songId : number) {
     if(songId) {
       this.router.navigate(['/song/songPage', songId]);
+    }
+  }
+
+  getAllAuthors(){
+    this.authorService.getAllAuthors().subscribe(
+      authors=>{
+        this.authors=authors
+      }, err=>{
+        console.log(err);
+      }
+    )
+  }
+
+  getAuthorName(authorId:number){
+    this.authorName=this.authors.find(x=>x.authorId===authorId).name;
+    return this.authorName;
+  }
+
+  redirectToAuthorPage(authorId: number) {
+    if (authorId > 0) {
+      this.router.navigate(['/author/authorPage', authorId]);
     }
   }
 
